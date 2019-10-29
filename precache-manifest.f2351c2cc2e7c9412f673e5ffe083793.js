43ae3ad8e1ccc6eaf5098886ac9615a5902d49b7
Tereshka <tereshka07@gmail.com>
2019-10-29 20:46:37 +0900

    Updates

---
+++ precache-manifest.f2351c2cc2e7c9412f673e5ffe083793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fdc8ab0c59d73bca551",
+    "url": "/vk-show-photos/static/js/runtime~main.a878b6dd.js"
+  },
+  {
+    "revision": "08f91f1436e2fb1a0ff7",
+    "url": "/vk-show-photos/static/js/main.2b34f71b.chunk.js"
+  },
+  {
+    "revision": "86d4b68a847b2abbc18d",
+    "url": "/vk-show-photos/static/js/2.8eb1fa21.chunk.js"
+  },
+  {
+    "revision": "08f91f1436e2fb1a0ff7",
+    "url": "/vk-show-photos/static/css/main.89b1e3d5.chunk.css"
+  },
+  {
+    "revision": "ffd43b29b1c23d752c97f60731c62f46",
+    "url": "/vk-show-photos/index.html"
+  }
+];
